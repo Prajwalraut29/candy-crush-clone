922fddf1e2005d671e32130b579aff1de5a3572b
prajwal raut <90103440+Prajwalraut29@users.noreply.github.com>
2023-12-12 22:18:27 +0530

    candy-crush

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { createBoard } from "./utils/createBoard";
+import Board from "./components/Board";
+import { moveBelow, updateBoard } from "./store";
+import { checkForRowOfFour, checkForRowOfThree, isColumnOfFour } from "./utils/moveCheckLogic";
+import { formulaForColumnOfFour, formulaForColumnOfThree, generateInvalidMoves } from "./utils/formulas";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const board = useAppSelector(({ candyCrush: { board } }) => board);
+  const boardSize = useAppSelector(({ candyCrush: { boardSize } }) => boardSize);
+
+  useEffect(() => {
+    // dispatch(updateBoard(createBoard(boardSize)))
+    dispatch(updateBoard(createBoard(boardSize)));
+  }, [boardSize, dispatch]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfThree(boardSize));
+      checkForRowOfFour(newBoard , boardSize, generateInvalidMoves(boardSize,true))
+      checkForRowOfThree(newBoard , boardSize,generateInvalidMoves(boardSize));
+      dispatch(updateBoard(newBoard))
+      dispatch(moveBelow())
+    }, 150);
+    return () => clearInterval(timeout)
+
+  }, [board, boardSize, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center h-screen">
+      <Board />
     </div>
   );
 }
---
+++ src/store/index.ts
@@ -0,0 +1,32 @@
+import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
+import { moveBelowReducer } from "./reducer/moveBelow";
+const initialState : {
+    board : string[];
+    boardSize : number;
+} = {
+    board :[],
+    boardSize:8,
+};
+
+const candyCrushSlice = createSlice({
+    name : "candyCrush",
+    initialState,
+    reducers:{
+   updateBoard : (state , action:PayloadAction<string[]>)=>{
+    state.board = action.payload;
+   },
+   moveBelow : moveBelowReducer,
+    }
+})
+
+export const store = configureStore({
+    reducer :{
+        candyCrush : candyCrushSlice.reducer,
+    },
+})
+
+export const {updateBoard , moveBelow} = candyCrushSlice.actions ;
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
